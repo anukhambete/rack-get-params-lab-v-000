37890630b3c71dcea3c4933f21ada0cd61ad2c3e
Anu Khambete <anukhambete@gmail.com>
2018-06-27 22:00:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,6 +17,8 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
+      else @@cart.each do |c|
+        resp.write "#{c}\n"
     else
       resp.write "Path Not Found"
     end
